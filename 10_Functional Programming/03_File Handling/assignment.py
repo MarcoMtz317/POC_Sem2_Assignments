7bf55851798785e404a49701ac1194ea5a9e4f88
Marco Martinez <martimar012@warren.k12.in.us>
2024-03-21 08:16:05 -0400

    assignemnt

---
+++ 10_Functional Programming/03_File Handling/assignment.py
@@ -0,0 +1,8 @@
+file_path = "C:\Users\martimar012\POC_Sem2_Assignments\10_Functional Programming\\03_File Handling\myFile.txt"
+
+try:
+    steam = open('file_patch')
+    "C:\Users\martimar012\POC_Sem2_Assignments\10_Functional Programming\\03_File Handling\myFile.txt"
+    steam.close()
+except Exception as e:
+    print('an error occured: ', e)
